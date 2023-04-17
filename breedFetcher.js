51032f98175c5287845629b4e5793dc8af72b590
Mathew Charm <mattcharmz@gmail.com>
2023-04-17 02:50:19 -0700

    json the cat has been refactored

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,23 @@
 const request = require('request');
-const breed = process.argv[2];
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-  if (error) {
-    console.log("error: ", error);
-  }
-  if (response) {
-    console.log("response: ", response.statusCode);
-  }
-  const data = JSON.parse(body);
-  console.log(data[0]);
-  console.log(typeof data);
-});
+
+const fetchBreedDescription = function(breed, callback) {
+
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+    if (error) {
+      console.log("error: ", error);
+    }
+    if (response) {
+      console.log("response: ", response.statusCode);
+    }
+    const data = JSON.parse(body);
+    
+    const breed = data[0];
+    if (breed) {
+      callback(null, breed.description);
+    } else {
+      callback(`failed to find ${breed}`, null);
+    }
+  });
+};
+  
+module.exports = { fetchBreedDescription };
